e719280b8c3c411dda12802c740e686729ad32cb
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2019-10-19 13:49:09 +0200

    Update task_7.c

--- ps1/task_7.c
+++ ps1/task_7.c
@@ -1,79 +1,105 @@
 #include <superkarel.h>
+void turn_right();
 void check();
+void doit();
 bool checkey();
 int main(){
-	turn_on("task_7.kw");
+	turn_on("task_7.kw"); 
 	set_step_delay(100);
-	while(checkey()== false){
+	while(front_is_clear()){
+		put_beeper();
 		step();
-		if(beepers_present()){check();
-		}
+	}
+	put_beeper();
+	turn_left();
+	turn_left();
+	while(front_is_clear()){
+		step();
+	}
+	turn_left();
+	turn_left();
+	while(front_is_clear()){
+	check();
+	if(checkey()){
+		pick_beeper();
+		pick_beeper();
+	}
+	turn_left();
+	turn_left();
+	if(front_is_clear()){
+	step();
+	}
+	while(left_is_blocked()){
+		step();
+	}
+	}
+	turn_left();
+	turn_left();
+	while (front_is_clear()){
+		pick_beeper();
+		step();
 	}
 	pick_beeper();
-	pick_beeper();
+	turn_left();
+	turn_left();
+	while(front_is_clear()){
+		if(beepers_present()){
+			turn_left();
+			step();
+			put_beeper();
+			turn_left();
+turn_left();
+			step();
+			turn_left();
+			pick_beeper();
+		}
+		step();
+	}
+	if(beepers_present()){
+			pick_beeper();
+			turn_left();
+			step();
+			turn_left();
+			turn_left();
+			put_beeper();
+			step();
+			turn_left();
+	}
 	turn_off();
 	return 0;
 }
-void check(){
-       	while(beepers_present()){
+bool checkey(){
 	pick_beeper();
-	if (no_beepers_present()){
-		while(not_facing_north()){
-			turn_left();
-		}
-	}
-	else pick_beeper();
-	if (no_beepers_present()){
-		while(not_facing_west()){
-				turn_left();
-				}
-				}
-	else pick_beeper();
-	if (no_beepers_present()){
-		while(not_facing_south()){
-			turn_left();
-		}
-	}
-	else pick_beeper();
-	if (no_beepers_present()){
-		while(not_facing_east()){
-			turn_left();
-		}
+	pick_beeper();
+	if(no_beepers_present()){
+        put_beeper();
+	put_beeper();
+	return false;
 	}
 	else put_beeper();
-}
-}
-bool checkey(){
-	if(no_beepers_present()){
-		return false;
-	}
-	else
-       	while(beepers_present()){
-	pick_beeper();
-	if (no_beepers_present()){
-		put_beeper();
-	}
-	else pick_beeper();
-	if (no_beepers_present()){
-		put_beeper();
-		put_beeper();
-				}
-	else pick_beeper();
-	if (no_beepers_present()){
-		put_beeper();
-		put_beeper();
-		put_beeper();
-	}
-	else pick_beeper();
-	if (no_beepers_present()){	
-		put_beeper();
-		put_beeper();
-		put_beeper();
-		put_beeper();
-	}
-	else
-		pick_beeper();
+	put_beeper();
 	return true;
-	}
-	
 }
+void turn_right(){
+	turn_left();
+	turn_left();
+	turn_left();
+}
+void check(){
+	put_beeper();
+	turn_left();
+	step();
+	while(no_beepers_present()){
+			step();
+			if(right_is_clear()){
+				turn_right();
+			}
+			if (front_is_blocked()){
+			while(front_is_blocked()){
+			turn_left();
+			}
+			}
+	}
+	put_beeper();
+}
+
