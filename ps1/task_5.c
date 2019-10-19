7d7deee09cf95490c242807eeccd7aaf875884d8
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2019-10-19 12:38:55 +0200

    Update task_5.c

--- ps1/task_5.c
+++ ps1/task_5.c
@@ -1,25 +1,106 @@
 #include <superkarel.h>
+void check();
+void kruh();
+void turn_right();
 int main(){
 	turn_on("task_5.kw");
 	set_step_delay(100);
+	while(not_facing_west()){
+		turn_left();
+	}
 	while(front_is_clear()){
 		step();
 	}
-	while (no_beepers_present()){
-	if (front_is_blocked()){
-		turn_left();
+	turn_left();
+	while(front_is_clear()){
+		step();
 	}
+	turn_left();
+	while(front_is_clear()){
+		step();
+		put_beeper();
+	}
+	while (beepers_present()){
+		check();
+	}
+	turn_left();
+	turn_left();
+	step();
+	turn_right();
+	while(front_is_clear()){
+		step();
+		put_beeper();
+	}
+	while(beepers_present()){
+		check();
+	}
+	turn_left();
+	turn_left();
+	step();
 	put_beeper();
-	step();
+	turn_left();
+	turn_left();
+	while(front_is_clear()){
+		step();
 	}
 	turn_left();
-	step();
+	kruh();
 	turn_left();
 	step();
-	step();
+	while(no_beepers_present()){
+		step();
+	}
+	pick_beeper();
 	while(not_facing_north()){
 		turn_left();
 	}
+
 	turn_off();
 	return 0;
 } 
+void check(){
+	while(beepers_present()){
+		turn_left();
+		turn_left();
+		pick_beeper();
+		if (no_beepers_present()){
+			step();
+		}
+		while(beepers_present()&& front_is_clear()){
+			step();
+			if (no_beepers_present()){
+				turn_left();
+				turn_left(); 
+				step();
+				pick_beeper();
+				step();
+			}
+		}
+	}
+}
+void kruh(){
+	while(no_beepers_present()){
+	if (front_is_blocked()){
+		turn_left();
+	put_beeper();
+	step();
+	put_beeper();
+	} 
+	else 
+		put_beeper();
+	step();
+	}
+		if (front_is_blocked()){
+			turn_left();
+			turn_left();
+			step();
+			pick_beeper();
+			turn_left();
+			turn_left();
+	}
+}
+void turn_right(){
+	turn_left();
+	turn_left();
+	turn_left();
+}
