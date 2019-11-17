8d6b5d2521c795464a4768e64a197dfae40052d6
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2019-11-17 15:10:27 +0100

    Update main.c

--- ps3/main.c
+++ ps3/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <hangman.h>
+#include "hangman.h"
 int main() {
 char secret[16];
 get_word(secret);
-hangman(secret;
+hangman(secret);
 return 0;
 }
