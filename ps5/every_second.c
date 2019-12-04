e562676880a978acfb2bd9eb5aa4aaca6e4a6c7e
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2019-12-04 20:08:10 +0100

    Update every_second.c

--- ps5/every_second.c
+++ ps5/every_second.c
@@ -7,25 +7,51 @@ int main (int argc, char* argv[]) {
 	char letter;
 	int c = 0;
 	int g = 0;
-	int sum = 0;
-	int l = 0;
 	int h = 0;
+	int s = 0;
+	int t = 0;
+	int a = 0;
+	int r = 0;
+	int d = 0;
+	int e = 0;
+	int w = 0;
+	int q = 0;
+	int sum = 0;
 	char word[500];
-	while (l != 1) {
+	while (sum != 1) {
 		letter = fgetc(fpointer);
-		if (letter == 'S' || letter == 'T' || letter == 'A' || letter == 'R'){
-			sum++;
-			continue;
+		if (letter == 'S'){
+			s++;
+		}    
+		if (letter == 'T'){
+			t++;
+		}	
+		if (letter == 'A'){
+			a++;
+		}	
+		if (letter == 'R'){
+			r++;
 		}
-		if (sum >= 5){
-		if (letter == 'O'){
-			l++;
-		}
-		       if(letter == 'P'){
-		}
+		if (s >= 1 && t >= 2 && a >= 1 && r >= 1){
+			if (letter == 'O'){
+				d++;
+			}
+			if(letter == 'P'){
+				e++;
+			}
 			if (letter == 'T'){
+				w++;
 			}
 			if (letter == 'S'){
+				q++;
+			}
+			if (q >= 1 && w >= 1 && e >= 1 && d >= 1){
+				sum = 1;
+				word[c - 3] = ' ';
+				word[c - 2] = ' ';
+				word[c - 1] = ' ';
+				word[c] = ' ';
+				break;
 			}
 			if (letter == ' ' || letter == EOF){
 				g++;
@@ -46,11 +72,10 @@ int main (int argc, char* argv[]) {
 	}
 	fclose(fpointer);
 	FILE *fpointerr= fopen("output.txt","w");
-	for (int p = 2; p < h; p++){
+	for (int p = 3; p < h; p++){
 		fputc(word[p],fpointerr);
 	}
 
 	fclose(fpointerr);
 	return 0;
 }
-
